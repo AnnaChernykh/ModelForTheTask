51fabed27486ef68ebe512b2912e906362e9d304
Anna Chernykh <annchernykh@gmail.com>
2017-06-04 00:31:17 +0300

    Класс схемы со свойствами зависимостей

---
+++ ModelForTheTask/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+
+
+namespace ModelForTheTask
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        
+        public MainWindow()
+        {
+            TaskScheme Scheme = new TaskScheme(); 
+            InitializeComponent();
+            Scheme.Number = 1;
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;data source=" + Environment.CurrentDirectory + "\\Task.mdb";
+            OleDbConnection myOleDbConnection = new OleDbConnection(connectionString);
+            OleDbCommand FindID = myOleDbConnection.CreateCommand();
+            FindID.CommandText = string.Format("SELECT Number, ID FROM Scheme WHERE Number=" + Scheme.Number);
+            myOleDbConnection.Open();
+            OleDbDataReader myOleDbDataReader1 = FindID.ExecuteReader();
+            myOleDbDataReader1.Read();
+            Scheme.ID = Convert.ToInt32(myOleDbDataReader1["ID"]);
+            myOleDbDataReader1.Close();
+            myOleDbConnection.Close();
+            MessageBox.Show(Scheme.Number + " " + Scheme.ID);
+        }
+    }
+}
---
+++ ModelForTheTask/TaskScheme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.OleDb;
+
+namespace ModelForTheTask
+{
+    public class TaskScheme : DependencyObject
+    {
+        public static readonly DependencyProperty SchemeNumber;
+        public static readonly DependencyProperty SchemeID;
+
+        static TaskScheme()
+        {
+            SchemeNumber = DependencyProperty.Register("Number", typeof(int), typeof(TaskScheme));
+            SchemeID = DependencyProperty.Register("ID", typeof(int), typeof(TaskScheme));
+        }
+        public int Number
+        {
+            get { return (int)GetValue(SchemeNumber); }
+            set { SetValue(SchemeNumber, value); }
+        }
+        public int ID
+        {
+            get { return (int)GetValue(SchemeID); }
+            set { SetValue(SchemeID, value); }
+        }
+     }
+
+    public class TaskParametrs:DependencyObject
+    {
+        public static readonly DependencyProperty Generator;
+        public static readonly DependencyProperty Transformer;
+    }
+}
